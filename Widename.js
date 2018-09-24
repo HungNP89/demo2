72cc59c6854f69486f35505b8c112db1dd3f478c
Pham Ngoc Hung <hungpham161100@gmail.com>
2018-09-24 11:48:04 +0300

    demo2

---
+++ Widename.js
@@ -0,0 +1,101 @@
+
+//  Widename.js (c) Kari Laitinen
+
+//  http://www.naturalprogramming.com
+
+//  2016-12-02 File created.   2016-12-02 Last modification.
+
+/*  In this program characters of a string are accessed with
+    an index value inside brackets [ ]. Another possibility would
+    be to use the charAt() method.
+*/
+
+process.stdout.write( "\n Please, type in your name: " ) ;
+
+process.stdin.on( "data", function( input_from_user )
+{
+   // When we take the string from the user input, we need to
+   // use the trim() method to get rid of line termination characters.
+   
+   var name_from_keyboard = input_from_user.toString().trim().toUpperCase() ;
+
+   process.stdout.write( "\n Here is your name in a wider form: \n\n  " ) ;
+
+   // In the following loops, character_index gets values
+   // 0, 1, 2, ... until the last possible index value.
+
+   for ( let character_index  in  name_from_keyboard )
+   {
+      process.stdout.write( " "  +   name_from_keyboard[ character_index ] ) ;
+   }
+
+   process.stdout.write( "\n\n The characters as hexadecimal codes: \n\n  " ) ;
+
+   for ( let character_index  in  name_from_keyboard )
+   {
+      // String method charCodeAt() returns the numerical Unicode.
+
+      var character_code = name_from_keyboard.charCodeAt( character_index ) ;
+
+      // Value 16 specifies that toString() must return a hexadecimal string.
+
+      process.stdout.write(  " "  +  character_code.toString( 16 ) ) ;
+   }
+    process.stdout.write( "\n\n The length of your name is : \n\n ");
+	
+   {
+	   process.stdout.write( " " + name_from_keyboard.length ) ;
+	   
+   }
+      process.stdout.write( "\n\n Here is your name in reverse order : \n\n ");
+	  
+	var alo = name_from_keyboard.length ;
+	
+	while ( alo > 0 ) 
+	{
+		alo --;
+		process.stdout.write ( name_from_keyboard[alo]);
+	}
+	
+	const array_of_morse_codes = 
+	
+	[ "A", ".-",   "B", "-...", "C", "-.-.", "D", "-..", "E", ".",
+   "F", "..-.", "G", " --.", "H", "....", "I", "..",  "J", ".---",
+   "K", "-.-",  "L", ".-..", "M", "--",   "N", "-.",  "O", "---",
+   "P", ".--.", "Q", "--.-", "R", ".-.",  "S", "...", "T", "-",   
+   "U", "..-",  "V", "...-", "W", ".--",  "X", "-..-","Y", " -.--", 
+   "Z", "--..", "1", ".----","2", "..---","3", "...--","4","....-",
+   "5", ".....","6", "-....","7", "--...","8", "---..","9","----.",
+   "0", "-----"," ", "     "  ] ;
+   
+   process.stdout.write ( "\n\n Your name in morse codes is : \n\n");
+    for ( let alo  =  0 ;
+             alo  <  name_from_keyboard.length ;
+             alo  ++ )
+   {
+      let index_of_character_in_array  =
+             array_of_morse_codes.indexOf( name_from_keyboard[ alo ] ) ;
+
+      if ( index_of_character_in_array  !=  -1 )
+      {
+         process.stdout.write( "   "  +  
+                   array_of_morse_codes[ index_of_character_in_array + 1 ] ) ;
+      }
+   }
+   
+   {
+   var characters_in_name = Array.from( name_from_keyboard ) ;
+   var random_character_index =
+                Math.floor( Math.random() * characters_in_name.length ) ;
+   var removed_character = characters_in_name[ random_character_index ] ;
+   characters_in_name.splice( random_character_index, 1 ) ; // remove the character
+   process.stdout.write( removed_character ) ;
+   }
+   process.stdout.write( "\n\n" ) ;
+   process.exit() ;
+} ) ;
+
+
+
+
+
